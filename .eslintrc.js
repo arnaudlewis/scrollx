b05e5c28d0a36be79fdf123383e6d3af35803d4f
Arnaud LEWIS <alw@zengularity.com>
2016-10-25 10:33:40 +0200

    fix tests

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  "env": {
+    "es6": true,
+    "node": true
+  },
+  "parser": "babel-eslint",
+  "extends": "eslint:recommended",
+  "rules": {
+    "no-console": 0,
+    "indent": [
+      "warn",
+      2
+    ],
+    "no-unused-vars": ["error", { "vars": "all", "args": "none" }],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "warn",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "never"
+    ]
+  },
+  "parserOptions": {
+    "sourceType": "module"
+  }
+};
