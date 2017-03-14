b684076f3c70f15b5137e413d1fe67ea618da28b
Arnaud lewis <alw@zengularity.com>
2017-03-14 12:21:44 +0100

    update build

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,42 @@
-module.exports = {
-     entry: './lib/scrollx.js',
-     output: {
-         path: './dist',
-         filename: './scrollx.js'
-     },
-     module: {
-         loaders: [{
-             test: /\.js$/,
-             exclude: /node_modules/,
-             loader: 'babel-loader'
-         }]
-     }
- };
+var webpack = require('webpack');
+var path = require('path');
+
+var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
+var env = process.env.WEBPACK_ENV;
+
+var libraryName = 'scrollx';
+var plugins = [];
+var outputFile;
+
+if (env === 'build') {
+  plugins.push(new UglifyJsPlugin({ minimize: true }));
+  outputFile = libraryName + '.min.js';
+} else {
+  outputFile = libraryName + '.js';
+}
+
+var config = {
+  entry: __dirname + '/lib/scrollx.js',
+  devtool: 'source-map',
+  output: {
+    path: __dirname + '/dist',
+    filename: outputFile,
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  },
+  module: {
+    loaders: [{
+         test: /\.js$/,
+         exclude: /node_modules/,
+         loader: 'babel-loader'
+     }]
+  },
+  resolve: {
+    root: path.resolve('./lib'),
+    extensions: ['', '.js']
+  },
+  plugins: plugins
+};
+
+module.exports = config;
